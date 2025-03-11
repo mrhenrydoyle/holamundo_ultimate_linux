9fdc732a5706cd5b4fd2bc27629cfb20897b3ad0
mrhenrydoyle <58106168+mrhenrydoyle@users.noreply.github.com>
2025-03-11 15:11:54 +0100

    Update pomodoro.sh

--- pomodoro.sh
+++ pomodoro.sh
@@ -2,7 +2,7 @@
 
 clear 
 read -p "Ingresa el tiempo que va a durar el pomodoro (en segundos): " tiempo 
-echo comenzando pomodoro! proximo descanso en $tiempo segundos." 
+echo "comenzando pomodoro! proximo descanso en $tiempo segundos." 
 sleep $tiempo
 
 espeak -ves "El tiempo ha terminado, ahora puedes descansar."
